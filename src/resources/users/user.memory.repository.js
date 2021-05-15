63b32596b4f45157c2691c0060ba57662fe0e73d
Maxim Kalinin <kalinkin.fiz@gmail.com>
2021-05-15 22:41:12 +0300

    feat: user memory implementation

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,22 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const User = require('./user.model');
+
+const Users = [
+  new User({ name: 'maxim', login: 'maxim', password: '12345qwerty' }),
+];
+
+const getAll = async () => Users;
+
+const getById = async (id) => Users.find((user) => user.id === id);
+
+const createUser = async ({ name, login, password }) => {
+  const user = new User({ name, login, password });
+  Users.push(user);
+  return user;
 };
 
-module.exports = { getAll };
+module.exports = {
+  Users,
+  getAll,
+  getById,
+  createUser,
+};
